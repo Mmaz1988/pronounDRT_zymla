2b41f4c4e742c078c73521b1c7029431fe119238
MMAZ <mark-matthias.zymla@uni-konstanz.de>
2023-02-03 07:41:07 +0100

    updated the lexicon and added the right file

--- englishLexicon.pl
+++ englishLexicon.pl
@@ -30,22 +30,22 @@ lexicon(det,_,[its],poss(nonhuman)).
    Nouns: lexicon(noun,Symbol,Words,{[],[Hypernym],Hypernym})
 ========================================================================*/
 
-lexicon(noun,witch,[witch],female).
-lexicon(noun,wizard,[wizard],male).
-lexicon(noun,human,[human],[organism]).
-lexicon(noun,female,[female],[human]).
-lexicon(noun,male,[male],[human]).
-
-%lexicon(noun,princess,[princess],female).
-%lexicon(noun,villain,[villain],human).
-%lexicon(noun,troll,[troll],human).
-%lexicon(noun,reeindeer,[reindeer],animal).
-%lexicon(noun,sleigh,[sleigh],[vehicle]).
-%lexicon(noun,snowman,[snowman],[organism]).
-%lexicon(noun,sunbath,[sunbath],[act]).
-
+lexicon(noun,witch,[witch],[female]).
+lexicon(noun,wizard,[wizard],[male]).
+%lexicon(noun,human,[human],[organism]).
+%lexicon(noun,female,[female],[human]).
+%lexicon(noun,male,[male],[human]).
 
 /*
+lexicon(noun,princess,[princess],female).
+lexicon(noun,villain,[villain],human).
+lexicon(noun,troll,[troll],human).
+lexicon(noun,reeindeer,[reindeer],animal).
+lexicon(noun,sleigh,[sleigh],[vehicle]).
+lexicon(noun,snowman,[snowman],[organism]).
+lexicon(noun,sunbath,[sunbath],[act]).
+
+
 lexicon(noun,abstraction,[abstraction],[top]).
 lexicon(noun,act,[act],[top]).
 lexicon(noun,animal,[animal],[organism]).
@@ -110,7 +110,23 @@ lexicon(pn,olaf,[olaf],male).
 lexicon(pn,christopher,[christopher],male).
 lexicon(pn,hans,[hans],male).
 
-
+/*
+lexicon(pn,butch,[butch],male).
+lexicon(pn,honey_bunny,[honey,bunny],male).
+lexicon(pn,jimmy,[jimmy],male).
+lexicon(pn,jody,[jody],female).
+lexicon(pn,jules,[jules],male).
+lexicon(pn,lance,[lance],male).
+lexicon(pn,marsellus,[marsellus],male).
+lexicon(pn,marsellus,[marsellus,wallace],male).
+lexicon(pn,marvin,[marvin],male).
+lexicon(pn,mia,[mia],female).
+lexicon(pn,mia,[mia,wallace],female).
+lexicon(pn,pumpkin,[pumpkin],male).
+lexicon(pn,thewolf,[the,wolf],male).
+lexicon(pn,vincent,[vincent],male).
+lexicon(pn,vincent,[vincent,vega],male).
+*/
 
 /*========================================================================
    Intransitive Verbs: lexicon(iv,Symbol,Words,{fin,inf})
@@ -119,11 +135,33 @@ lexicon(pn,hans,[hans],male).
 lexicon(iv,pretty,[is,pretty],fin).
 lexicon(iv,blush,[blushes],fin).
 
+/*
+lexicon(iv,collapse,[collapses],fin).
+lexicon(iv,collapse,[collapse],inf).
+lexicon(iv,dance,[dances],fin).
+lexicon(iv,dance,[dance],inf).
+lexicon(iv,die,[dies],fin).
+lexicon(iv,die,[die],inf).
+lexicon(iv,growl,[growls],fin).
+lexicon(iv,growl,[growl],inf).
+lexicon(iv,okay,[is,okay],fin).
+lexicon(iv,outoftown,[is,out,of,town],fin).
+lexicon(iv,married,[is,married],fin).
+lexicon(iv,playairguitar,[plays,air,guitar],fin).
+lexicon(iv,playairguitar,[play,air,guitar],inf).
+lexicon(iv,smoke,[smokes],fin).
+lexicon(iv,smoke,[smoke],inf).
+lexicon(iv,snort,[snorts],fin).
+lexicon(iv,snort,[snort],inf).
+lexicon(iv,shriek,[shrieks],fin).
+lexicon(iv,shriek,[shriek],inf).
+lexicon(iv,walk,[walks],fin).
+lexicon(iv,walk,[walk],inf).
+*/
 
 /*========================================================================
    Transitive Verbs: lexicon(tv,Symbol,Words,{fin,inf})
 ========================================================================*/
-
 
 lexicon(tv,love,[loves],fin).
 lexicon(tv,love,[love],inf).
@@ -143,6 +181,43 @@ lexicon(tv,create,[created],fin).
 lexicon(tv,create,[create],inf).
 lexicon(tv,melt,[melts],fin).
 lexicon(tv,melt,[melt],inf).
+
+/*
+lexicon(tv,clean,[cleans],fin).
+lexicon(tv,clean,[clean],inf).
+lexicon(tv,drink,[drinks],fin).
+lexicon(tv,drink,[drink],inf).
+lexicon(tv,date,[dates],fin).
+lexicon(tv,date,[date],inf).
+lexicon(tv,discard,[discards],fin).
+lexicon(tv,discard,[discard],inf).
+lexicon(tv,eat,[eats],fin).
+lexicon(tv,eat,[eat],inf).
+lexicon(tv,enjoy,[enjoys],fin).
+lexicon(tv,enjoy,[enjoy],inf).
+lexicon(tv,hate,[hates],fin).
+lexicon(tv,hate,[hate],inf).
+lexicon(tv,have,[has],fin).
+lexicon(tv,have,[have],inf).
+lexicon(tv,donewith,[is,done,with],fin).
+lexicon(tv,kill,[kills],fin).
+lexicon(tv,kill,[kill],inf).
+lexicon(tv,know,[knows],fin).
+lexicon(tv,know,[know],inf).
+lexicon(tv,like,[likes],fin).
+lexicon(tv,like,[like],inf).
+lexicon(tv,love,[loves],fin).
+lexicon(tv,love,[love],inf).
+lexicon(tv,pickup,[picks,up],fin).
+lexicon(tv,pickup,[pick,up],inf).
+lexicon(tv,shoot,[shot],fin).
+lexicon(tv,shoot,[shoot],inf).
+lexicon(tv,tell,[told],fin).
+lexicon(tv,tell,[tell],inf).
+lexicon(tv,worksfor,[works,for],fin).
+lexicon(tv,worksfor,[work,for],inf).
+
+*/
 
 /*========================================================================
    Copula
